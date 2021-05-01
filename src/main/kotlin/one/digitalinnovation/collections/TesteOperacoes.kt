1e2f9c80decf7affecaf6b4396f7f60580944e07
lrhonorato <lucas.rh@gmail.com>
2021-04-30 22:59:47 -0300

    created double array operations test class (max, min, average and filter)

---
+++ src/main/kotlin/one/digitalinnovation/collections/TesteOperacoes.kt
@@ -0,0 +1,18 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    val salarios = doubleArrayOf(1000.0, 2250.0, 4000.0)
+
+    for (salario in salarios) {
+        println(salario)
+    }
+
+    println("---------------------")
+    println("Maior salário : ${salarios.max()}")
+    println("Menor salário : ${salarios.min()}")
+    println("Média salarial : ${salarios.average()}")
+
+    println("---------------------")
+    val salariosMaiorQue2500 = salarios.filter { it > 2500.0 }
+    salariosMaiorQue2500.forEach { println(it) }
+}
