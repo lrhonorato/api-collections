d6420afd3529ea0f66435a017b6a6a30964844b2
lrhonorato <lucas.rh@gmail.com>
2021-05-03 00:04:35 -0300

    updated test list, extract employee to separated file and created test set

---
+++ src/main/kotlin/one/digitalinnovation/collections/Funcionario.kt
@@ -0,0 +1,13 @@
+package one.digitalinnovation.collections
+
+data class Funcionarioo(
+    val nome: String,
+    val salario: Double,
+    val tipoContratacao: String
+) {
+    override fun toString(): String =
+        """
+            Nome:    $nome
+            Salário: $salario
+        """.trimIndent()
+}
